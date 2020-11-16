b35b3c1b7cef844bbd3ed57d79974fa352b04ad8
Megan Wong <meganwong117@gmail.com>
2020-11-16 17:38:16 +0000

    add column to dresses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_161151) do
+ActiveRecord::Schema.define(version: 2020_11_16_173500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_161151) do
     t.string "event_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
     t.index ["user_id"], name: "index_dresses_on_user_id"
   end
 
