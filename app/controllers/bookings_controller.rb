a851c64cb6195e864ca670d54ab167c1ca2b351f
Noemie Lachman <noemie.lachman@hotmail.fr>
2020-11-17 14:29:14 +0000

    Cancel booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,7 +13,12 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 
-  # def booking_params
-  #   params.require(:booking).permit(:dress_id, :user_id, :created_date, :end_date)
-  # end
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+  end
+
+  def booking_params
+    params.require(:booking).permit(:dress_id, :user_id, :created_date, :end_date)
+  end
 end
