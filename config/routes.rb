6b2680f43d568682a439a3019e9bb44792f3f028
Noemie Lachman <noemie.lachman@hotmail.fr>
2020-11-17 15:47:37 +0000

    Added the cancel booking feature

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   resources :dresses do
     resources :bookings, only: [ :new, :create ]
   end
-  resources :bookings, only: [ :index]
+  resources :bookings, only: [ :index, :destroy]
 end
