d6c65d341766f52d8317cf62706b9664f8c01f1b
Noemie Lachman <noemie.lachman@hotmail.fr>
2020-11-19 17:06:06 +0000

    accept decline feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_114625) do
+ActiveRecord::Schema.define(version: 2020_11_19_161801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_114625) do
     t.date "end_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status", default: "pending"
     t.index ["dress_id"], name: "index_bookings_on_dress_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
